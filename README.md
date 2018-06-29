16b4f49529ce88a14dcb391caaa47f9e0c98810d
Jonathan Castro dos Santos <jonathandossantos.c@gmail.com>
2018-06-29 16:57:26 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Restaurant-review
+# Restaurant Reviews - Front-end Web Nanodegree
+
+## About the project
+
+This project simulates a real working environment. An incomplete code was provided and totally without accessibility. The goal was to turn the web application into a responsive and accessible page for as many people as possible. In addition, prioritizing the offline first (PWA) was one of the requirements required by Nanodegree.
+
+## Steps to execute the project
+
+* Run Restaurant-Review-00.zip and extract all files.
+* Now is the time to emulate a local server. For this you will need to have python installed on your machine, if you do not have it, insert it.
+* Now, on your terminal run the command "python -m SimpleHTTPServer 8000" (without the quotation marks). If you're already using "port 8000" you can configure another port in the /js/dbhelper.js file.
+* Open your browser and go to: http://localhost:8000 (change "8000" if you are using another port). Enjoy the web application.
+
+Created by Jonathan Santos
---
+++ sw.js
@@ -0,0 +1,72 @@
+'use strict';
+
+/*
+ *Adapted from https://developers.google.com/web/ilt/pwa/lab-caching-files-with-service-worker
+ */
+
+let filesToCache = [
+  '.',
+  'css/styles.css',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'index.html',
+  'restaurant.html',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'data/restaurants.json'
+];
+
+let staticCacheName = 'pages-cache-v1';
+
+// Caches the application's shell
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Updates the cache
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [staticCacheName];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          //delete outdated caches
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+// Check network files and return cached files
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request);
+    })
+  );
+});
+
